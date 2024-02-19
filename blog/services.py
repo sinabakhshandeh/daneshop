d193dcfdb4b0f617ae851e112bc6d3cbb8d6e6ab
sinabakhshandeh <sina.bakhshandeh.t@gmail.com>
2024-02-19 15:42:55 +0330

    feat(blog): add pagination to posts

--- blog/services.py
+++ blog/services.py
@@ -1,8 +1,18 @@
-from django.db import models
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import QuerySet
 
 from blog.models import Post
 
 
-def post_list_view() -> models.QuerySet:
+def post_list_view(page: int) -> QuerySet:
     posts = Post.objects.filter(status="published")
+    paginator = Paginator(posts, 10)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        posts = paginator.page(paginator.num_pages)
     return posts
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from blog import services
 
 
 def post_list_view(request):
-    posts = services.post_list_view()
+    page = request.GET.get("page", 1)
+
+    posts = services.post_list_view(page=page)
     context = {"posts": posts}
     return render(request, "blog/post_list_view.html", context)
