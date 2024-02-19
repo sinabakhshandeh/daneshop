b1653a924786dd39619ba58f4913b404fbc69926
sinabakhshandeh <sina.bakhshandeh.t@gmail.com>
2024-02-19 08:19:20 +0330

    feat: add post and category model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+from daneshop.models import BaseModel
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    parent = models.ForeignKey(
+        "self",
+        null=True,
+        blank=True,
+        related_name="children",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Post(BaseModel):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    published_at = models.DateTimeField(null=True, blank=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ("-published_at",)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
