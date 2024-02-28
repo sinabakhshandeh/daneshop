9e2a3dc1449caec546089d25f162b92cc9a2b386
sinabakhshandeh <sina.bakhshandeh.t@gmail.com>
2024-02-28 20:47:44 +0330

    fix: medai model name change

--- shop/admin.py
+++ shop/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from shop.models import Media, Product, ProductCategory, ProductPrice
+from shop.models import Product, ProductCategory, ProductMedia, ProductPrice
 
 admin.site.register(Product)
 admin.site.register(ProductCategory)
 admin.site.register(ProductPrice)
-admin.site.register(Media)
+admin.site.register(ProductMedia)
