5d3f6b4ed65ea285617444369ff541d70700eb0a
sinabakhshandeh <sina.bakhshandeh.t@gmail.com>
2024-02-29 15:48:31 +0330

    feat: add a tag to calculate cart price

--- cart/templatetags/cart_tags.py
+++ cart/templatetags/cart_tags.py
@@ -9,3 +9,11 @@ register = template.Library()
 def cart_items_count(cart_uuid: str):
     cart = Cart.objects.get(uuid=cart_uuid)
     return CartItem.objects.all().filter(cart=cart).count()
+
+
+@register.simple_tag
+def cart_total_price(cart_uuid: str):
+    cart = Cart.objects.get(uuid=cart_uuid)
+    items = CartItem.objects.all().filter(cart=cart)
+    prices = (item.total_price() for item in items)
+    return sum(prices)
