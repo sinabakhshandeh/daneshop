fb12794b4809e4c0d84d94d2fa9d78b335448979
sinabakhshandeh <sina.bakhshandeh.t@gmail.com>
2024-02-29 14:24:01 +0330

    update: add app to settings

--- daneshop/settings.py
+++ daneshop/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "blog",
     "shop",
+    "cart",
 ]
 
 MIDDLEWARE = [
@@ -126,8 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = "app/static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
