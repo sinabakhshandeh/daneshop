e991fdba4893e8770b9705b4c41659067af0a648
sinabakhshandeh <sina.bakhshandeh.t@gmail.com>
2024-02-29 10:27:05 +0330

    update: add app name

--- shop/urls.py
+++ shop/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from shop import views
 
+app_name = "shop"
+
 urlpatterns = [
-    path("", views.shop_product_list_view),
+    path("", views.shop_product_list_view, name="home"),
 ]
